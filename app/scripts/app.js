9d006cd7152c717ed84bf1bff915959467a9ae1f
Ben Meline <benmeline@gmail.com>
2015-04-06 10:40:47 -0500

    Angular setup

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uCartAppApp
+ * @description
+ * # uCartAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uCartAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uCartAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uCartAppApp
+ */
+angular.module('uCartAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
