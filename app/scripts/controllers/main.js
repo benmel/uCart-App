105df460e316e776caae57add8c1e2efa18184e4
Ben Meline <benmeline@gmail.com>
2015-04-20 16:46:14 -0500

    Calculate total

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,25 +15,42 @@ angular.module('uCartAppApp')
     $scope.quantity = 0;
     $scope.price = 0;
 
+    $scope.subtotal = 0;
+    $scope.tax = 0;
+    $scope.total = 0;
+
     $scope.addItem = function(){
 	    var item = {
 	        itemName: $scope.itemName,
 	        quantity: $scope.quantity,
 	        price: $scope.price
 	    };
-	    if (item.itemName && item.quantity>0 && item.price >0)
-	    {
-	    $scope.items.push(item);
-		}
-		else
-		{
-			window.alert('not valid');
+	    if (item.itemName && item.quantity>0 && item.price >0) {
+	    	$scope.subtotal += (item.quantity * item.price);
+	    	$scope.tax = calculateTax($scope.subtotal);
+	    	$scope.total = $scope.subtotal + $scope.tax;
+				$scope.items.push(item);
 
-		}
+	    	$scope.itemName = '';
+    		$scope.quantity = 0;
+    		$scope.price = 0;
+			} else {
+				window.alert('not valid');
+			}
 
 		};
 
 		$scope.removeItem = function(index){
+			var item = $scope.items[index];
+			$scope.subtotal -= (item.quantity * item.price);
+	    $scope.tax = calculateTax($scope.subtotal);
+	    $scope.total = $scope.subtotal + $scope.tax;
 			$scope.items.splice(index, 1);
 		};
-  }); 
+  
+	var calculateTax = function(subtotal) {
+		return Math.round(subtotal * 0.0925 * 100) / 100;
+	};	
+
+  });
+
