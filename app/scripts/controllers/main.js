da47a7c4105fa982d1f7a79b4f4b855c03635e52
Ben Meline <benmeline@gmail.com>
2015-04-20 15:45:36 -0500

    Remove item button

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,23 +9,22 @@
  */
 angular.module('uCartAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.items = [
-    {itemName: 'banana', quantity: '1', price:'5'},
-    {itemName: 'tomato', quantity: '2', price:'2'}
-    ];
+    $scope.items = [];
 
     $scope.itemName = '';
     $scope.quantity = 0;
     $scope.price = 0;
 
     $scope.addItem = function(){
-    var item = {
-        itemName: $scope.itemName,
-        quantity: $scope.quantity,
-        price: $scope.price
-    };
-    $scope.items.push(item);
-	
-	};
+	    var item = {
+	        itemName: $scope.itemName,
+	        quantity: $scope.quantity,
+	        price: $scope.price
+	    };
+	    $scope.items.push(item);
+		};
 
+		$scope.removeItem = function(index){
+			$scope.items.splice(index, 1);
+		};
   }); 
