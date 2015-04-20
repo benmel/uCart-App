61e03dd4a131d20c3ccdaedde606a5b83bc3e6e3
lukeahn1224 <ahnnuga@gmail.com>
2015-04-20 15:21:28 -0500

    adding rows

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,23 @@
  */
 angular.module('uCartAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.item = '';
+    $scope.items = [
+    {itemName: 'banana', quantity: '1', price:'5'},
+    {itemName: 'tomato', quantity: '2', price:'2'}
+    ];
+
+    $scope.itemName = '';
     $scope.quantity = 0;
     $scope.price = 0;
 
-    $scope.scanItem = function() {
-    	window.alert($scope.item + ' ' + $scope.quantity + ' ' + $scope.price);
+    $scope.addItem = function(){
+    var item = {
+        itemName: $scope.itemName,
+        quantity: $scope.quantity,
+        price: $scope.price
     };
+    $scope.items.push(item);
+	
+	};
+
   }); 
