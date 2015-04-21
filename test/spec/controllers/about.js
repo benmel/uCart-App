75b05425472ef03a2564c73ce423f0290e09c3f9
Ben Meline <benmeline@gmail.com>
2015-04-20 23:20:37 -0500

    Add untracked changes

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('uCartAppApp'));
+  beforeEach(module('uCartApp'));
 
   var AboutCtrl,
     scope;
