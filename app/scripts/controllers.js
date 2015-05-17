ab1b72032ab24b66a8a713e1d5d17716e2902950
lukeahn1224 <ahnnuga@gmail.com>
2015-05-17 12:54:27 -0500

    groceryList added. need some styling

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -46,9 +46,59 @@ angular.module('starter.controllers', [])
 	};
 })
 
-.controller('ListCtrl', function($scope) {
-	$scope = null;
+.controller('ListCtrl', function($scope, CartItems, IdVerification) {
+  
+
+  $scope.items = CartItems.all();
+  $scope.item = { id: null, name: null, quantity: null, price: null };
+  
+  $scope.subtotal = CartItems.getSubtotal;
+  $scope.tax = CartItems.getTax;
+  $scope.total = CartItems.getTotal;
+
+  $scope.showVerificationNeeded = IdVerification.showVerificationNeeded;
+  $scope.showPaymentOptions = IdVerification.showPaymentOptions;
+  $scope.code = { input: null } ;
+  
+  $scope.add = function(itemNameInput) {
+    var itemName = itemNameInput;
+    var itemArray = { id:null, name:itemName, quantity:null, price:null};
+     if (itemArray.name) {
+      alert(itemArray.name);
+      CartItems.add(itemArray);
+      $scope.item = { id: null, name: null, quantity: null, price: null };
+    } else {
+      window.alert('Item not valid');
+    }
+  };
+
+  $scope.remove = function(item) {
+    CartItems.remove(item);
+  };
+
+   $scope.list = [
+    {'name': 'Banana',
+     'snippet': 'Full of potassium',
+     'age': 1},
+    {'name': 'Milk',
+     'snippet': 'Get your calcium',
+     'age': 2},
+    {'name': 'Chicken breast',
+     'snippet': 'ultimate protean source',
+     'age': 3},
+     {'name': 'Apple',
+     'snippet': 'an apple a day keep doctors away',
+     'age': 4},
+     {'name': 'Oranges',
+     'snippet': 'from Valencia. period.',
+     'age': 5}
+  ];
+
+  $scope.orderProp = 'age';
+
+
 })
+
 
 .controller('CouponsCtrl', function($scope) {
 	$scope = null;
