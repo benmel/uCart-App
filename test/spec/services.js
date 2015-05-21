5005199ffd198658a7e0de898c100109a0098a64
Ben Meline <benmeline@gmail.com>
2015-05-21 15:44:00 -0500

    Test for add barcode

--- test/spec/services.js
+++ test/spec/services.js
@@ -36,22 +36,44 @@ describe('Services: CartItems', function() {
 		});
   });
 
+  describe('add', function() {
+  	beforeEach(function() {
+  		var barcode = '123456789';
+  		CartItems.add(barcode);
+  	});	
+  	it('should add to items array', function() {
+  		CartItems.all().should.have.length(1);
+  	});
+  	it('should give an id to item', function() {
+  		var newItem = CartItems.all()[0];
+  		newItem.id.should.not.be.null;
+  	})
+  	it('should update subtotal, tax and total', function() {
+  		CartItems.getSubtotal().should.equal(1);
+  		CartItems.getTax().should.equal(1*0.0925);
+  		CartItems.getTotal().should.equal(1+1*0.0925);
+  	});
+  });
+
   describe('addInput', function() {
   	beforeEach(function() {
   		var item = { id: null, name: 'Banana', quantity: 5, price: 1 };
   		CartItems.addInput(item);
   	});
-  	it('should add to items array if id is null', function() {
-  		CartItems.all().should.have.length(1);
-  	});
-  	it('should give an id to item if id is null', function() {
-  		var newItem = CartItems.all()[0];
-  		newItem.id.should.not.be.null;
-  	})
-  	it('should update subtotal, tax and total if id is null', function() {
-  		CartItems.getSubtotal().should.equal(5);
-  		CartItems.getTax().should.equal(5*0.0925);
-  		CartItems.getTotal().should.equal(5+5*0.0925);
+
+  	describe('id is null', function() {
+	  	it('should add to items array if id is null', function() {
+	  		CartItems.all().should.have.length(1);
+	  	});
+	  	it('should give an id to item if id is null', function() {
+	  		var newItem = CartItems.all()[0];
+	  		newItem.id.should.not.be.null;
+	  	})
+	  	it('should update subtotal, tax and total if id is null', function() {
+	  		CartItems.getSubtotal().should.equal(5);
+	  		CartItems.getTax().should.equal(5*0.0925);
+	  		CartItems.getTotal().should.equal(5+5*0.0925);
+	  	});
   	});
 
   	describe('id is not null', function() {
@@ -186,5 +208,4 @@ describe('Services: IdVerification', function() {
 			IdVerification.showVerificationNeeded().should.be.false;
 		});
 	});
-
 });
